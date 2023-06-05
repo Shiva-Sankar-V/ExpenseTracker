59b9def7335479b1d29d118ab3b970e2e4c980cf
shiva <shivasankarvenkat1996@gmail.com>
2023-06-05 21:09:56 +0530

    added the structure for the api

--- store/expenses-context.js
+++ store/expenses-context.js
@@ -1,3 +1,8 @@
 import { createContext, useReducer } from "react";
 
-export const ExpensesContext = createContext({});
+export const ExpensesContext = createContext({
+  expenses: [],
+  addExp: ({ description, amt, date }) => {},
+  delExp: (id) => {},
+  updateExp: (id, { description, amt, date }) => {},
+});
