871ef3bfda336aedee6edf80ea63dcad234a19b2
shiva <shivasankarvenkat1996@gmail.com>
2023-06-05 19:19:05 +0530

    add delete button & style it for manage exp screen

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,5 +1,7 @@
 import { useLayoutEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import IconButton from "../components/ExpensesOutput/UI/IconButton";
+import { GlobalStyles } from "../constants/styles";
 
 const ManageExpense = ({ route, navigation }) => {
   const editedExpId = route.params?.expId;
@@ -11,13 +13,36 @@ const ManageExpense = ({ route, navigation }) => {
     });
   }, [navigation, isEdit]);
 
+  const deleteExpHandler = () => {};
+
   return (
-    <View>
-      <Text>ManageExpense</Text>
+    <View style={styles.container}>
+      {isEdit && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            color={GlobalStyles.colors.error500}
+            size={36}
+          />
+        </View>
+      )}
     </View>
   );
 };
 
 export default ManageExpense;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: "center",
+  },
+});
