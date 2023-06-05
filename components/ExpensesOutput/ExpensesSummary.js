a93df2698d34598d19de811d6b57a0306337e405
shiva <shivasankarvenkat1996@gmail.com>
2023-06-05 11:18:40 +0530

    improving styling for summary

--- components/ExpensesOutput/ExpensesSummary.js
+++ components/ExpensesOutput/ExpensesSummary.js
@@ -1,4 +1,5 @@
 import { StyleSheet, Text, View } from "react-native";
+import { GlobalStyles } from "../../constants/styles";
 
 const ExpensesSummary = (props) => {
   const sum1 = props.expenses.reduce((sum, exp) => {
@@ -11,13 +12,37 @@ const ExpensesSummary = (props) => {
   // second argument 0 will be assigned to sum for the first time running the function, otherwise sum will be assigned to undefined
   //Argument "exp" will copy the data from the expenses props.
   return (
-    <View>
-      <Text>{props.periodName}</Text>
-      <Text>Rs {sum1.toFixed(2)}</Text>
+    <View style={styles.outerContainer}>
+      <View style={styles.contrainer}>
+        <Text style={styles.period}>{props.periodName}</Text>
+        <Text style={styles.sum}>Rs {sum1.toFixed(2)}</Text>
+      </View>
     </View>
   );
 };
 // to fixed is a built-in js code which will reduce the decimal place to the no given inside the brackets
 export default ExpensesSummary;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  outerContainer: {
+    alignItems: "center",
+  },
+  contrainer: {
+    padding: 8,
+    backgroundColor: GlobalStyles.colors.primary50,
+    borderRadius: 8,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "99%",
+  },
+  period: {
+    fontSize: 12,
+    color: GlobalStyles.colors.primary400,
+  },
+  sum: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: GlobalStyles.colors.primary500,
+  },
+});
