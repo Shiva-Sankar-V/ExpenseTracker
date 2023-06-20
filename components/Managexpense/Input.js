7a65ce6c952fc09443f72f720dd0d75311d2d1a6
Shiva Sankar <shivasankar19@outlook.com>
2023-06-20 14:40:42 +0530

    Comments to simplify the approach

--- components/Managexpense/Input.js
+++ components/Managexpense/Input.js
@@ -12,3 +12,5 @@ const Input = ({ label, type, maxLen }) => {
 export default Input;
 
 const styles = StyleSheet.create({});
+//Since the properties required for textinput is more and by doing as above
+// we will be adding more and more cusrom props to our custom component just to forward them to our text input component.
