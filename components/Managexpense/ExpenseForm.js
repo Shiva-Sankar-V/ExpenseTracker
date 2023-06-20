1d598efc8c4bbffcc90b1be95c033da9f61ae41d
Shiva Sankar <shivasankar19@outlook.com>
2023-06-20 17:21:27 +0530

    providing properteies for the amt and date inputs

--- components/Managexpense/ExpenseForm.js
+++ components/Managexpense/ExpenseForm.js
@@ -2,11 +2,25 @@ import { StyleSheet, Text, View } from "react-native";
 import Input from "./Input";
 
 const ExpenseForm = () => {
+  function amountChangeHandler() {}
   return (
     <View>
-      <Input label="Amount" />
-      <Input label="Date" />
-      <Input label="Description" />
+      <Input
+        label="Amount"
+        textInputConfig={{
+          keyboardType: "decimal-pad",
+          onChangeText: amountChangeHandler,
+        }}
+      />
+      <Input
+        label="Date"
+        textInputConfig={{
+          placeholder: "YYYY-MM-DD",
+          maxLength: 10,
+          onChangeText: () => {},
+        }}
+      />
+      <Input label="Description" textInputConfig={{}} />
     </View>
   );
 };
