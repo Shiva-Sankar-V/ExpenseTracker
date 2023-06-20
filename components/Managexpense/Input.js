e79b8e4e49b6aeae75584d38b48e93abaebff5f4
Shiva Sankar <shivasankar19@outlook.com>
2023-06-20 14:48:24 +0530

    Changed the approach by simplifying the code

--- components/Managexpense/Input.js
+++ components/Managexpense/Input.js
@@ -1,10 +1,10 @@
 import { StyleSheet, Text, TextInput, View } from "react-native";
 
-const Input = ({ label, type, maxLen }) => {
+const Input = ({ label, textInputConfig }) => {
   return (
     <View>
       <Text>{label}</Text>
-      <TextInput keyboardType={type} maxLength={maxLen} />
+      <TextInput {...textInputConfig} />
     </View>
   );
 };
@@ -14,3 +14,8 @@ export default Input;
 const styles = StyleSheet.create({});
 //Since the properties required for textinput is more and by doing as above
 // we will be adding more and more cusrom props to our custom component just to forward them to our text input component.
+
+// Simple way for doing is to get a generic custom prop and then speard them to text input.
+// We will then provide the prop as object and the property names used in that object should match
+// the property names supported by TextInput and the values for those properties then are simply values accepeted
+// by the props we can set on TextInput
