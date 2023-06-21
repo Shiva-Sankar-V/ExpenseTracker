ad2bc67462079cde4524713738fc73bf57d01787
Shiva Sankar <shivasankar19@outlook.com>
2023-06-21 17:20:11 +0530

    state updating function for all inputs

--- components/Managexpense/ExpenseForm.js
+++ components/Managexpense/ExpenseForm.js
@@ -1,8 +1,21 @@
 import { StyleSheet, Text, View } from "react-native";
 import Input from "./Input";
+import { useState } from "react";
 
 const ExpenseForm = () => {
-  function amountChangeHandler() {}
+  const [inputVal, setInputVal] = useState({
+    amount: "",
+    date: "",
+    description: "",
+  });
+  function inputChangeHandler(inputIdentifier, enteredVal) {
+    setInputVal((curInputVal) => {
+      return {
+        ...curInputVal,
+        [inputIdentifier]: enteredVal,
+      };
+    });
+  }
   return (
     <View style={styles.form}>
       <Text style={styles.title}>Your Expenses</Text>
@@ -11,7 +24,8 @@ const ExpenseForm = () => {
           label="Amount"
           textInputConfig={{
             keyboardType: "decimal-pad",
-            onChangeText: amountChangeHandler,
+            onChangeText: inputChangeHandler.bind(this, "amount"),
+            value: inputVal.amount,
           }}
         />
         <Input
@@ -19,7 +33,8 @@ const ExpenseForm = () => {
           textInputConfig={{
             placeholder: "YYYY-MM-DD",
             maxLength: 10,
-            onChangeText: () => {},
+            onChangeText: inputChangeHandler.bind(this, "date"),
+            value: inputVal.date,
           }}
         />
       </View>
@@ -28,6 +43,8 @@ const ExpenseForm = () => {
         textInputConfig={{
           multiline: true,
           autoCapitalize: "sentences",
+          onChangeText: inputChangeHandler.bind(this, "description"),
+          value: inputVal.description,
         }}
       />
     </View>
